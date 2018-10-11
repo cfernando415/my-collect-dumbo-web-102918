804d3cd3643023597e0e8e55a92e2818d962e975
Carlo Fernando <ca.fernando415@gmail.com>
2018-10-11 20:43:16 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,17 @@
+def my_collect(arr)
+  if block_given? 
+    i = 0
+    
+    while i < arr.length
+      (yield(arr[i]))
+      i += 1
+    end
+  else
+    "No block was given"
+  end
+  arr
+end
 
-
+my_collect(["ruby", "javascript", "python", "objective-c"]) do |lang|
+  lang.upcase
+end
