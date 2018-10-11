f774678928fb9bad701e6acc3958eedbc73a0cec
Carlo Fernando <ca.fernando415@gmail.com>
2018-10-11 20:44:03 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,13 +1,13 @@
 def my_collect(arr)
-  if block_given? 
+  #if block_given? 
     i = 0
     
     while i < arr.length
       (yield(arr[i]))
       i += 1
     end
-  else
-    "No block was given"
-  end
+ # else
+ #   "No block was given"
+ # end
   arr
 end
