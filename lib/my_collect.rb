38545867838312769170cdb791d1e7f9a85e5a81
Carlo Fernando <ca.fernando415@gmail.com>
2018-10-11 20:48:11 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,9 @@
 def my_collect(arr)
   #if block_given? 
     i = 0
-    
+    result = []
     while i < arr.length
-      yield(arr[i])
+      result << yield(arr[i])
       i += 1
     end
  # else
